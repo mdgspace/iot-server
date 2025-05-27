c533afd25c8108f2f4514593e9d6cd168b146aff
LordRyuga <saketishaan123@gmail.com>
2025-05-27 08:51:38 +0530

    fix: allows only one key per year instead of multiple entries in db

--- controllers/keyHolders.js
+++ controllers/keyHolders.js
@@ -4,9 +4,24 @@ export const addHolder = async (req, res) => {
     const {enroll_num} = req.body;
     // console.log(head);
     console.log(req.body);
+    const enroll_num_str = enroll_num.toString();
+    const yearPrefix = enroll_num_str.substring(0, 2);
+    
     try{
+        const doesExist = await pool.query(
+            'SELECT * FROM keyHolders WHERE enrollment_num ~ ($1)', [`^${yearPrefix}`]
+        )
+
+        if(doesExist && doesExist.rowCount > 0)
+        {
+            await pool.query(
+                'DELETE FROM keyHolders WHERE CAST(enrollment_num AS TEXT) ~ $1',
+                [`^${yearPrefix}`]
+            );
+        }
+
         const result = await pool.query(
-            'INSERT INTO activeUsers (enroll_num) VALUES ($1) RETURNING *', [enroll_num]
+            'INSERT INTO keyHolders (enroll_num) VALUES ($1) RETURNING *', [enroll_num]
         );
 
         res.status(201).json(result.rows[0]);
