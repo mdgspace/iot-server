33d996fa2c50cf4b5ef2f84eb798b16a98e8da73
Siddharth Chopra <contact.sid.chopra@gmail.com>
2025-07-16 20:12:36 +0530

    chore: deploy to vercel

--- controllers/labBot.js
+++ controllers/labBot.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { WebClient } from '@slack/web-api';
+
 dotenv.config();
 const slackClient = new WebClient(process.env.LAB_BOT_TOKEN);
 const BotToken = process.env.LAB_BOT_TOKEN;
@@ -13,29 +15,67 @@ let currentBroStatus = true;
 
 function reverseBroStatus() {
     currentBroStatus = !currentBroStatus
-    return currentBroStatus ? messageOpen: messageClose
+    return currentBroStatus ? messageOpen : messageClose
 }
 
-export const listenToGraphReq = async (req, res)=>{
-    
+export const listenToGraphReq = async (req, res) => {
+
 }
 
-export const printFromBro = async (req, res) => 
-{
+export const handleAppMention = async (event) => {
+    const { text, channel, user, ts } = event;
+
+    console.log(text);
+
+    // const event_time = parseEventTime(text);
+    // console.log('Event time:', event_time);
+    // const emoji = parseEmoji(text);
+
+    // if (!event_time) {
+    //     console.log(' Message does not contain valid event time');
+    //     return;
+    // }
+    // if (!emoji) {
+    //     console.log('Message does not contain valid emoji')
+    //     return;
+    // }
+
+    // const createEventObj =
+    // {
+    //     message: text,
+    //     channel: channel,
+    //     created_by: user,
+    //     event_time: event_time,
+    //     emoji: emoji,
+    //     ts: ts
+    // }
+
+    // try {
+    //     const result = await createEvent(createEventObj);
+
+    //     console.log(' Event stored in DB:', result.rows[0]);
+    //     const message = "Event successfully created!";
+    //     postMessageToSlack(message, createEventObj.channel);
+    // } catch (err) {
+    //     console.error(' Error storing event:', err);
+    // }
+};
+
+
+
+export const printFromBro = async (req, res) => {
     let message = reverseBroStatus();
     console.log("Bro status changed to: " + message);
-    try
-    {
+    try {
         const result = await postMessageToSlack(message);
 
-        return res.status(200).json({ 
-            success: true, 
+        return res.status(200).json({
+            success: true,
             message: "Message posted to Slack successfully.",
             slackResponse: result
         });
 
-    }catch (error) 
-    {
+    } catch (error) {
         console.error('Error posting message to Slack:', error);
         return res.status(500).json({ error: 'Failed to post message to Slack' });
     }
@@ -44,8 +84,7 @@ export const printFromBro = async (req, res) =>
 
 const postMessageToSlack = async (message) => {
     let response = null;
-    try
-    {
+    try {
         response = await axios.post(process.env.BASE_URL, {
             "channel": LABOPENCLOSE_CHANNEL_ID,
             "text": message,
@@ -55,8 +94,7 @@ const postMessageToSlack = async (message) => {
                 'Authorization': `Bearer ${SLACK_TOKEN}`,
             },
         });
-    }catch (error)
-    {
+    } catch (error) {
         console.error('Error posting message to Slack:', error);
     }
 
--- routes/slackRoutes.js
+++ routes/slackRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { createEventAdapter } from '@slack/events-api';
 import dotenv from 'dotenv';
-import { handleAppMention, handleReactionAdded, handleReactionRemoved } from '../controllers/eventController.js';
+import { handleAppMention_event, handleReactionAdded, handleReactionRemoved } from '../controllers/eventController.js';
+import { handleAppMention_labbot } from '../controllers/labBot.js';
 
 dotenv.config();
 
@@ -12,7 +13,8 @@ router.use('/events', slackEvents.expressMiddleware());
 // Handle app mentions
 slackEvents.on('app_mention', async (event) => {
   console.log('App mentioned:', event.text);
-  await handleAppMention(event);
+  await handleAppMention_event(event);
+  await handleAppMention_labbot(event);
 });
 slackEvents.on('reaction_added', async (event) => {
   console.log('Reacted to app');
